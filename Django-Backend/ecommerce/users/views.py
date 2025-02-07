d19586097996ce5ee913141c9020afddb1d761d8
GaneshJC0 <ganeshjchoudhari@gmail.com>
2025-02-07 13:59:19 +0530

    updated views

--- Django-Backend/ecommerce/users/views.py
+++ Django-Backend/ecommerce/users/views.py
@@ -8,8 +8,7 @@ import json
 from cart.cart import Cart
 from rest_framework import generics
 
-
-
+# Register User
 def register_user(request):
     if request.method == 'POST':
         form = CustomUserRegistrationForm(request.POST)
@@ -24,7 +23,7 @@ def register_user(request):
         form = CustomUserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
 
-
+# Login User
 def login_user(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -34,18 +33,18 @@ def login_user(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                
+
                 current_user = Profile.objects.get(user__id=request.user.id)
                 saved_cart = current_user.old_cart
                 if saved_cart:
                     converted_cart = json.loads(saved_cart)
-                    
+
                     cart = Cart(request)
                     for key, value in converted_cart.items():
                         cart.db_add(product=key, quantity=value)
 
                 messages.info(request, 'Login successful!')
-                return redirect('home')  
+                return redirect('home')
             else:
                 messages.error(request, "Invalid username or password.")
         else:
@@ -54,21 +53,20 @@ def login_user(request):
         form = AuthenticationForm()
     return render(request, 'users/login.html', {'form': form})
 
-
+# Logout User
 def logout_user(request):
     logout(request)
     messages.success(request, ('You have been logged out!!!'))
     return redirect('home')
 
-
+# Update User
 def update_user(request):
     if request.user.is_authenticated:
         current_user = CustomUser.objects.get(id=request.user.id)
-        user_form = UpdateUserForm(request.POST or None, instance=current_user) 
-        
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
         if user_form.is_valid():
             user_form.save()
-            
             login(request, current_user)
             messages.success(request, "User Details updated")
             return redirect('home')
@@ -77,12 +75,12 @@ def update_user(request):
         messages.error(request, "You must be logged in to update your details")
         return redirect('home')
 
-
+# Update User Info
 def update_info(request):
     if request.user.is_authenticated:
         current_user = Profile.objects.get(user__id=request.user.id)
         form = UpdateInfoForm(request.POST or None, instance=current_user)
-        
+
         if form.is_valid():
             form.save()
             messages.success(request, "Your Profile info has been updated")
@@ -92,13 +90,13 @@ def update_info(request):
         messages.error(request, "You must be logged in to update your info")
     return render(request, 'users/update_info.html')
 
-
+# Update User Password
 def update_password(request):
     if request.user.is_authenticated:
         current_user = request.user
         if request.method == "POST":
-            form = UpdateUserPassword(current_user ,request.POST) 
-            
+            form = UpdateUserPassword(current_user, request.POST)
+
             if form.is_valid():
                 form.save()
                 messages.success(request, "Your password has been updated. Login with your new password")
@@ -107,7 +105,7 @@ def update_password(request):
                 for error in list(form.errors.values()):
                     messages.error(request, error)
                 return redirect('update_password')
-                     
+
         else:
             form = UpdateUserPassword(current_user)
             return render(request, 'users/update_password.html', {'form': form})
@@ -115,12 +113,24 @@ def update_password(request):
         messages.error(request, "You must be logged in to update your password")
         return redirect('home')
 
-
-
+# User Profile View
 def user_profile(request):
-    return render(request, 'users/user_profile.html')
+    if request.user.is_authenticated:
+        current_user = request.user
+        # Get the user profile, including the unique_id
+        user_data = {
+            'email': current_user.email,
+            'first_name': current_user.first_name,
+            'last_name': current_user.last_name,
+            'unique_id': current_user.unique_id,  # Fetch unique_id
+        }
 
+        return render(request, 'users/user_profile.html', {'user_data': user_data})
+    else:
+        messages.error(request, "You must be logged in to view your profile")
+        return redirect('login')
 
+# Shipping Information View
 def shipping_info(request):
     if request.user.is_authenticated:
         try:
@@ -136,16 +146,10 @@ def shipping_info(request):
                 shipping_address.save()
                 messages.success(request, "Your shipping information has been updated.")
                 return redirect('home')
-            # return render(request, 'users/shipping_information.html', {'form': form})
         else:
             form = ShippingAddressForm(instance=current_user)
-        
+
         return render(request, 'users/shipping_information.html', {'form': form})
     else:
         messages.error(request, "You must be logged in to update your info.")
         return redirect('login')
-
-
-
-
-

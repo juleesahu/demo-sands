e76dc08b894974f44b9370ee1712b8529f0b6c57
GaneshJC0 <ganeshjchoudhari@gmail.com>
2025-02-13 14:36:09 +0530

    updated

--- Django-Backend/ecommerce/ecommerce/urls.py
+++ Django-Backend/ecommerce/ecommerce/urls.py
@@ -11,7 +11,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Bellamore API",
+      title="Sales & Service API",
       default_version='v1',
       description="Backend Documentation",
       terms_of_service="https://www.google.com/policies/terms/",

4f3921a476d474adf7a966cc633285a5c1b36e0e
GaneshJC0 <ganeshjchoudhari@gmail.com>
2025-04-05 13:39:05 +0530

    added databse schema

--- Django-Backend/ecommerce/ecommerce/settings.py
+++ Django-Backend/ecommerce/ecommerce/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
+RAZORPAY_KEY_ID = 'rzp_test_w9IZjqFewUYiAk'
+RAZORPAY_KEY_SECRET = 'Ks1VA61iBDUzf3bYb3bAN103'
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get('SECRET_KEY'),
 
@@ -60,6 +61,7 @@ INSTALLED_APPS = [
     'api',
     'drf_yasg',
     'mlmtree',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [

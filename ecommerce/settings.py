99c9dfcd5ace3bdc53af121bea023032c1616f32
GaneshJC0 <ganeshjchoudhari@gmail.com>
2025-04-05 18:40:53 +0530

    updaetd

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -101,41 +101,38 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-#MYSQL
-
-# DATABASES = {
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.mysql',
-# 		'NAME': 'bellamore',
-# 		'USER': 'root',
-# 		'PASSWORD': 'Ra4kell@2020',
-# 		'HOST':'localhost',
-# 		'PORT':'3306',
-# 	}
-# }
-
-
-# DEPLOYING TO RAILWAY  -  POSTGRESQL
-
-# DATABASES = {
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.postgresql',
-# 		'NAME': 'railway',
-# 		'USER': 'postgres',
-# 		'PASSWORD': os.environ.get('POSTGESQL_PASSWORD'),
-#         # 'PASSWORD': os.environ['POSTGESQL_PASSWORD'],   //production
-# 		'HOST':'roundhouse.proxy.rlwy.net',
-# 		'PORT':'18007',
-# 	}
-# }
+'''
+# PostgreSQL Database Configuration on RDS
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'djan.postgresqlgo.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+}
+'''
+# PostgreSQL Database Configuration on RDS
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-db',
+        'USER': 'postgres',
+        'PASSWORD': 'Ganesh123',
+        'HOST': 'django-db.cr8osgscmzdq.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 
 
